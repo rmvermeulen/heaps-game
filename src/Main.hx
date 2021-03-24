8d6efcbf9e3b0e27bcf91eb3b414eb079e34d29a
Rasmus Vermeulen <rasmus_vermeulen@tutanota.com>
2021-03-24 13:40:05 +0100

    some code

--- src/Main.hx
+++ src/Main.hx
@@ -1,10 +1,18 @@
+import sys.io.File;
+
 class Main extends hxd.App {
 	override function init() {
-		var text = new h2d.Text(hxd.res.DefaultFont.get(), s2d);
-		text.text = "Hello World!";
+		var label = new h2d.Text(hxd.res.DefaultFont.get(), s2d);
+		label.text = "Hello World!";
 	}
 
 	static function main() {
+		try {
+			var data = File.getContent("data.json");
+			trace("Data:", data);
+		} catch (error) {
+			trace("Failed to open file", error);
+		}
 		new Main();
 	}
 }
